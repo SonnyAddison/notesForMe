627c72046cbd76ed263d5b889db88509881b73c3
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-08-04 14:04:37 -0700

    Found error in code that was not allowing for all to work

--- routers/api.js
+++ routers/api.js
@@ -4,11 +4,26 @@ const fs = require('fs');
 let uniqid = require('uniqid');
 
 module.exports = (app) => {
-
+    //get all notes
     app.get('/api/notes', (req, res) => {        
         res.sendFile(path.join(__dirname, '../db/db.json'));
     });
-    
+
+    //Get single note
+    app.get('/api/notes/:id', (req, res) => {
+      if (req.param.note_id) {
+        const noteId = req.params.note_id;
+        for(let i = 0; i < notes.length; i++) {
+            const currentNote = notes[i];   
+            if (currentNote.note_id === noteId) {
+                res.json(currentNote);
+                return;
+            }
+        }    
+      }
+    }
+    );
+    //Create new note    
     app.post('/api/notes', (req, res) => {
         console.log('setting up new note');
         let db = fs.readFileSync('db/db.json');
@@ -19,17 +34,17 @@ module.exports = (app) => {
             text: req.body.text,
             note_id: uniqid()
         };
-
+        //add new note to db
         db.push(newNote);
         fs.writeFileSync('db/db.json', JSON.stringify(db));
         res.send(db);
     });
-
+    //Delete note
     app.delete('/api/notes/:id', (req, res) => {
         let db = JSON.parse(fs.readFileSync('db/db.json'));
-        let deleteNote = db.filter(item => item.id !== req.params.id);
-        fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
-        res.json(deleteNote);      
+        let deleteNotes = db.filter(item => item.id !== req.params.note_id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);      
     });
 }
 
